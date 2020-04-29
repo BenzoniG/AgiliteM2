24e35732026701eb56bca29e5bf5f54b5545c30b
BenzoniG <43956080+BenzoniG@users.noreply.github.com>
2020-04-29 14:34:21 +0200

    Add files via upload

---
+++ Test Packages/MaireTest.java
@@ -0,0 +1,92 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Classe-test testMaire.
+ *
+ * @author  (votre nom)
+ * @version (un numéro de version ou une date)
+ *
+ * Les classes-test sont documentées ici :
+ * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
+ * et sont basées sur le document Š 2002 Robert A. Ballance intitulé
+ * "JUnit: Unit Testing Framework".
+ *
+ * Les objets Test (et TestSuite) sont associés aux classes à tester
+ * par la simple relation yyyTest (e.g. qu'un Test de la classe Name.java
+ * se nommera NameTest.java); les deux se retrouvent dans le męme paquetage.
+ * Les "engagements" (anglais : "fixture") forment un ensemble de conditions
+ * qui sont vraies pour chaque méthode Test à exécuter.  Il peut y avoir
+ * plus d'une méthode Test dans une classe Test; leur ensemble forme un
+ * objet TestSuite.
+ * BlueJ découvrira automatiquement (par introspection) les méthodes
+ * Test de votre classe Test et générera la TestSuite conséquente.
+ * Chaque appel d'une méthode Test sera précédé d'un appel de setUp(),
+ * qui réalise les engagements, et suivi d'un appel à tearDown(), qui les
+ * détruit.
+ */
+public class MaireTest
+{
+    private Ville springfield;
+    private Maire joe;
+
+    // Définissez ici les variables d'instance nécessaires à vos engagements;
+    // Vous pouvez également les saisir automatiquement du présentoir
+    // à l'aide du menu contextuel "Présentoir --> Engagements".
+    // Notez cependant que ce dernier ne peut saisir les objets primitifs
+    // du présentoir (les objets sans constructeur, comme int, float, etc.).
+    
+
+    /**
+     * Constructeur de la classe-test testMaire
+     */
+    public MaireTest()
+    {
+    }
+
+    /**
+     * Met en place les engagements.
+     *
+     * Méthode appelée avant chaque appel de méthode de test.
+     */
+    @Before
+    public void setUp() // throws java.lang.Exception
+    {
+        springfield = new Ville("Springfield", 30720);
+        joe = new Maire("Quimby", "Joe");
+    }
+
+    /**
+     * Supprime les engagements
+     *
+     * Méthode appelée après chaque appel de méthode de test.
+     */
+    @After
+    public void tearDown() // throws java.lang.Exception
+    {
+        //Libérez ici les ressources engagées par setUp()
+    }
+
+
+    @Test
+    public void testMaire()
+    {
+        assertEquals("Springfield", joe.dirige(springfield));
+        assertEquals(joe, springfield.getMaire());
+    }
+
+    @Test
+    public void testElection()
+    {
+        assertEquals("Joe Quimby", springfield.elire(joe));
+        assertEquals(springfield, joe.getVille());
+    }
+}
+
+
+
+
+
+
---
+++ Test Packages/PaysTest.java
@@ -0,0 +1,53 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author  guill
+ */
+public class PaysTest
+{
+    private Ville paris;
+    private Ville perpignan;
+    private Pays france;    
+    
+    public PaysTest(){}
+
+    // Met en place les engagements. Méthode appelée avant chaque appel de méthode de test.
+    @Before
+    public void setUp() // throws java.lang.Exception
+    {
+        paris = new Ville("Paris", 2148000);
+        perpignan = new Ville("Perpignan", 121934);
+        france = new Pays("France", 643801, paris);
+        france.ajouterVille(perpignan);
+        paris.setPays(france);
+        perpignan.setPays(france);
+    }
+
+    //Supprime les engagements. Méthode appelée après chaque appel de méthode de test.
+    @After
+    public void tearDown() // throws java.lang.Exception
+    {
+        //Libérez ici les ressources engagées par setUp()
+    }
+    
+    
+    @Test
+    public void testCapitale()
+    {
+        assertEquals(paris, france.getCapitale());
+        assertEquals(france, paris.getPays());
+        assertEquals(france, perpignan.getPays());
+        assertEquals(true, france.getVilles().contains(paris));
+        assertEquals(true, france.getVilles().contains(perpignan));
+        assertNotEquals(perpignan, france.getCapitale());
+    }
+}
+
+
+
+
+
+
