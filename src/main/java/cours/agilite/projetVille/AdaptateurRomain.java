4989aa7f4f9761d8d59aac18709e65163a1e1e70
BenzoniG <guillaume.benzoni@dauphine.eu>
2020-05-31 10:24:04 +0200

    MAJ adaptateur Romain

--- src/main/java/cours/agilite/projetVille/AdaptateurRomain.java
+++ src/main/java/cours/agilite/projetVille/AdaptateurRomain.java
@@ -1,5 +1,32 @@
 package cours.agilite.projetVille;
 
-public class AdaptateurRomain {
+import java.util.ArrayList;
+import java.util.Date;
 
+import cours.agilite.projetVille.gaulois.Gaulois;
+import cours.agilite.projetVille.gaulois.Romain;
+
+public class AdaptateurRomain extends Habitant 
+{
+	private Romain romain;
+
+	public AdaptateurRomain(String prenom, String nom, String sexe, Ville ville, int vitesse , int force, Gaulois gaulois) 
+	{
+		super(prenom, nom, sexe, ville);
+
+		this.romain = new Romain(vitesse, force, gaulois);
+	}
+	
+	public Romain getRomain() { return this.romain; }
+    public int getForce(){ return this.romain.getForce(); }
+    public int getVitesse(){ return this.romain.getVitesse(); }
+
+    public void  setForce(int force){ this.romain.setForce(force); }
+    public void  setVitesse(int vit){ this.romain.setVitesse(vit); }
+	public void setEnnemi(Gaulois ennemi) { this.romain.setEnnemi(ennemi);}
+
+    public boolean peutAttaquerEnnemi()
+    {
+        return this.romain.peutAttaquerEnnemi();
+    }
 }
